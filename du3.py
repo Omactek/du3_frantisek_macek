2fa4386a58ba28fb88c15bf49bc4b62142b8b2e4
František Macek <frantisek.macek1@gmail.com>
2021-12-15 14:31:49 +0100

    pouze volne dostupne kontejnery

--- du3.py
+++ du3.py
@@ -1,8 +1,8 @@
 import json
 import os
 
-kont_coord = []
-kont_id = []
+kont = []
+kont_volne = []
 
 try:
     if os.path.getsize("kontejnery.geojson") == 0:
@@ -16,10 +16,12 @@ with open("kontejnery.geojson", encoding="utf8") as kinfile:
         kontejnery = json.load(kinfile)
 
 for feature in kontejnery['features']:
-    kont_coord.append([feature["geometry"]["coordinates"][1],feature["geometry"]["coordinates"][1],feature["properties"]["STATIONNAME"],feature["properties"]["PRISTUP"]])
+    kont.append([feature["geometry"]["coordinates"][1],feature["geometry"]["coordinates"][1],feature["properties"]["STATIONNAME"],feature["properties"]["PRISTUP"]])
 
-print(kont_coord)
+for i in range(len(kont)):
+    if kont[i][3] == "volně":
+        kont_volne.append(kont[i][0:3])
 
-
+print(len(kont),len(kont_volne))
 
 
