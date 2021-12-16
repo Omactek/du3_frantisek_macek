e895ecbf8213050d7b29f5e8fcf50a73ef230cb0
František Macek <frantisek.macek1@gmail.com>
2021-12-16 16:08:45 +0100

    vypocitani vzdalenosti

--- du3.py
+++ du3.py
@@ -4,10 +4,11 @@ from pyproj import Transformer
 
 kont = [] #[[x,y,stationname,pristup]]
 kont_volne = []
-adr = []
-adr_krovak = []
+adr = [] #[[x,y,housenumber,street]]
 kontejnery_path = "kontejnery.geojson"
 adresy_path = "adresy.geojson"
+adresa_vzdalenost = []
+
 
 def load_file(file_path, file, file_name): #načte vstupní soubor a ověří, jestli existuje, nebo je prázdný
     try:
@@ -17,15 +18,15 @@ def load_file(file_path, file, file_name): #načte vstupní soubor a ověří, j
     except FileNotFoundError:
         print(f"Chybí vstupní soubor {file}.")
         quit()
-    with open("kontejnery.geojson", encoding="utf8") as file_name:
-        kontejnery = json.load(file_name)
-        return kontejnery
+    with open(file_path, encoding="utf8") as file_name:
+        data = json.load(file_name)
+        return data
 
 #načtení vstupního souboru kontejnerů
 kontejnery = load_file(kontejnery_path, "kontejnery", "kinfile") #načte vstupní soubor a ověří, jestli existuje, nebo je prázdný
 
 for feature in kontejnery["features"]: #načte do listu jen relevatní položky
-    kont.append([feature["geometry"]["coordinates"][1],feature["geometry"]["coordinates"][1],feature["properties"]["STATIONNAME"],feature["properties"]["PRISTUP"]])
+    kont.append([feature["geometry"]["coordinates"][0],feature["geometry"]["coordinates"][1],feature["properties"]["STATIONNAME"],feature["properties"]["PRISTUP"]])
 
 for i in range(len(kont)): #zvolí jen volně dostupné kontejnery
     if kont[i][3] == "volně":
@@ -34,7 +35,27 @@ for i in range(len(kont)): #zvolí jen volně dostupné kontejnery
 #načtení vstupního souboru adres
 adresy = load_file(adresy_path, "adresy", "adinfile")
 
-for feature in adresy["features"]:
-    adr.append([feature["geometry"]["coordinates"][1],feature["geometry"]["coordinates"][2],feature["properties"]["addr:housenumber"],feature["properties"]["addr:street"]])
+for feature in adresy["features"]: #načte do listu jen relevatní položky
+    adr.append([feature["geometry"]["coordinates"][0],feature["geometry"]["coordinates"][1],feature["properties"]["addr:housenumber"],feature["properties"]["addr:street"]])
+
+#převedení souřadnic do s-jtsk
+wgs84_sjtsk = Transformer.from_crs("epsg:4326","epsg:5514")
+for i in range(len(adr)):
+    adr[i][0] = wgs84_sjtsk.transform(adr[i][0],adr[i][1])
+    adr[i][1] = adr[i][0][1]
+    adr[i][0] = adr[i][0][0]
+
+#vypočítání vzdálenosti
+def euclidean_distance(coord_1_x,coord_1_y,coord_2_x, coord_2_y):
+    distance = pow(pow(coord_1_x - coord_2_x, 2) + pow(coord_1_y - coord_2_y, 2),0.5)
+    return distance
+
+for i in range(len(adr)):
+    adresa_vzdalenost.append([euclidean_distance(adr[i][0],adr[i][1],kont_volne[0][0],kont_volne[0][1]),adr[i][2],adr[i][3]])
+    for z in range(len(kont_volne)):
+        temp_dist = euclidean_distance(adr[i][0],adr[i][1],kont_volne[z][0],kont_volne[z][1])
+        if temp_dist < adresa_vzdalenost[i][0]:
+            adresa_vzdalenost[i][0] = temp_dist
 
 print(adr)
+print(kont_volne)
