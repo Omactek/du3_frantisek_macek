13d590f3605d792dd383aeeda2ea1f6d1755cfbe
František Macek <frantisek.macek1@gmail.com>
2021-12-16 12:40:40 +0100

    nacitani adres

--- du3.py
+++ du3.py
@@ -1,27 +1,40 @@
 import json
 import os
+from pyproj import Transformer
 
-kont = []
+kont = [] #[[x,y,stationname,pristup]]
 kont_volne = []
+adr = []
+adr_krovak = []
+kontejnery_path = "kontejnery.geojson"
+adresy_path = "adresy.geojson"
 
-try:
-    if os.path.getsize("kontejnery.geojson") == 0:
-        print("Vstupní soubor je prázdný")
+def load_file(file_path, file, file_name): #načte vstupní soubor a ověří, jestli existuje, nebo je prázdný
+    try:
+        if os.path.getsize(file_path) == 0:
+            print(f"Vstupní soubor {file} je prázdný.")
+            quit()
+    except FileNotFoundError:
+        print(f"Chybí vstupní soubor {file}.")
         quit()
-except FileNotFoundError:
-    print("Chybí vstupní soubor")
-    quit()
+    with open("kontejnery.geojson", encoding="utf8") as file_name:
+        kontejnery = json.load(file_name)
+        return kontejnery
 
-with open("kontejnery.geojson", encoding="utf8") as kinfile:
-        kontejnery = json.load(kinfile)
+#načtení vstupního souboru kontejnerů
+kontejnery = load_file(kontejnery_path, "kontejnery", "kinfile") #načte vstupní soubor a ověří, jestli existuje, nebo je prázdný
 
-for feature in kontejnery['features']:
+for feature in kontejnery["features"]: #načte do listu jen relevatní položky
     kont.append([feature["geometry"]["coordinates"][1],feature["geometry"]["coordinates"][1],feature["properties"]["STATIONNAME"],feature["properties"]["PRISTUP"]])
 
-for i in range(len(kont)):
+for i in range(len(kont)): #zvolí jen volně dostupné kontejnery
     if kont[i][3] == "volně":
         kont_volne.append(kont[i][0:3])
 
-print(len(kont),len(kont_volne))
+#načtení vstupního souboru adres
+adresy = load_file(adresy_path, "adresy", "adinfile")
 
+for feature in adresy["features"]:
+    adr.append([feature["geometry"]["coordinates"][1],feature["geometry"]["coordinates"][2],feature["properties"]["addr:housenumber"],feature["properties"]["addr:street"]])
 
+print(adr)
